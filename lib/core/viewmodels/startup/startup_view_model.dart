d160ef4cf069d0fee99e3aaa04ce268404c2c629
hendi <hendiappid@gmail.com>
2021-03-31 08:14:45 +0700

    added flutter_hooks packages for experiment reusable state widget

--- lib/core/viewmodels/startup/startup_view_model.dart
+++ lib/core/viewmodels/startup/startup_view_model.dart
@@ -1,5 +1,4 @@
 import 'package:mobile_template/shared/size_config.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:stacked/stacked.dart';
 import 'package:mobile_template/app/app.locator.dart';
 import 'package:mobile_template/core/services/localization_service.dart';
--- lib/ui/views/startup/start_up_view.dart
+++ lib/ui/views/startup/start_up_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:stacked/stacked.dart';
-
 import '../../../core/viewmodels/startup/startup_view_model.dart';
 import '../base_view.dart';
 import 'local_widgets/body.dart';
 
 class StartUpView extends StatelessWidget {
+
   @override
   Widget build(context) => ViewModelBuilder<StartUpViewModel>.nonReactive(
       viewModelBuilder: () => StartUpViewModel(),
